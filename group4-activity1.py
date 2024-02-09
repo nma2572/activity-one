0923d8e9f8be087b9bfadc376de0dac8ea2cf1f5
nma2572 <156881066+nma2572@users.noreply.github.com>
2024-02-09 17:32:47 +0400

    second draft

--- group4-activity1.py
+++ group4-activity1.py
@@ -1,6 +1,11 @@
 import turtle
+"""
+This function draws a square with a given side length, fill color and border color.
+"""
 
 def square(turta,square_color,border_color):
+    
+    turta = 90
     turtle.color(border_color)
     turtle.fillcolor(square_color)
     turtle.begin_fill()
@@ -14,15 +19,26 @@ def square(turta,square_color,border_color):
     turtle.left(90)
     turtle.end_fill()
 
+"""
+This function draws a circle with a given radius, fill color and border color.
+"""
 
 def circle(turta,circle_color,border_color):
+    
+    turta = 50
     turtle.color(border_color)
     turtle.fillcolor(circle_color)
     turtle.begin_fill()
     turtle.circle(turta)
     turtle.end_fill()
 
+"""
+This function draws a hexagon with a given side length, fill color and border color.
+"""
+
 def hexagon(turta,hex_color,border_color):
+    turta = 50
+    
 
     turtle.color(border_color)
     turtle.fillcolor(hex_color)
@@ -40,14 +56,22 @@ def hexagon(turta,hex_color,border_color):
     turtle.forward(turta)
     turtle.end_fill()
 
+"""
+This function sets the turtle's position to the given x and y coordinates.
+"""
 
-def setPos(heading,x,y):
-    turtle.setheading(heading)
+def setPos(turta,x,y):
+    
+    turtle.setheading(turta)
     turtle.penup()
     turtle.goto(x,y)
     turtle.pendown()
 
-def pattern(hex_color,circle_color,square_color,border_color):
+"""
+This function draws a pattern of shapes with the given colors.
+"""
+
+def pattern(turta,hex_color,circle_color,square_color,border_color):
     setPos(0,-320,101.699)
     hexagon(50,hex_color,border_color)
     setPos(0,-145,95)
@@ -69,14 +93,20 @@ def pattern(hex_color,circle_color,square_color,border_color):
     setPos(0,250,-195)
     square(90,square_color,border_color)
 
+
+"""
+This is the main function that prompts the user for input and draws the pattern.
+"""
+
 def main(): 
+    
     l = input("Enter the color of Hexagon: ")
     m = input("Enter the color of Circle: ")
     n = input("Enter the color of Square: ")
     o = input("Enter the color of shape borders: ")
     turtle.bgcolor("light blue")
     turtle.pensize(3)
-    pattern(l,m,n,o)
+    pattern(turtle,l,m,n,o)
     turtle.done()
 
 main()
